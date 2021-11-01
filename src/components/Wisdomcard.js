202667112032d48928f81049cc9324762b4aabaa
onvanessaproductdevelopment <product.development@onvanessa.com>
2021-11-01 21:08:20 +0100

    changed card style

--- src/components/Wisdomcard.js
+++ src/components/Wisdomcard.js
@@ -7,7 +7,7 @@ import React from 'react';
 const Wisdomcard = ({data}) => { //destructuring
 
     return (
-        <div className ='tc bg-black dib pa3 ma3 bw5 shadow-5 br4 mw5'>
+        <div className ='tc near-black bg-light-silver pa3 ma3 bw5 shadow-5 br4 mw5'>
            <div>
                 <p className ='fw1 f6 lh-copy mw5 '>{data}</p>
             </div>

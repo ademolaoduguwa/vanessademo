70e28abb82f86b2456d761c0b5d6692720fd7f0c
onvanessaproductdevelopment <product.development@onvanessa.com>
2021-11-01 17:53:14 +0100

    content added

--- src/roll.js
+++ src/roll.js
@@ -2,15 +2,15 @@
 import React from 'react'
 
 const roll = [
-    {
-      info:"you are cool"  
-    },
-    {
-        info:"you are wise"  
-     },
-     {
-        info:"you are not small"  
-     },
+    // {
+    //   info:"you are cool"  
+    // },
+    // {
+    //     info:"you are wise"  
+    //  },
+    //  {
+    //     info:"you are not small"  
+    //  },
 ]; //Holds roll of wisdom created and fetched from pythnayhwere
 
 export default roll;

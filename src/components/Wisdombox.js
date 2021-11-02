45898d1de03717f3ed42b3a244c03b6dfdb04fe3
onvanessaproductdevelopment <product.development@onvanessa.com>
2021-11-02 08:01:57 +0100

    updated moreusecase

--- src/components/Wisdombox.js
+++ src/components/Wisdombox.js
@@ -10,7 +10,7 @@ const Wisdombox = ({dropWisdom}) => {
             type='input' 
             style= {{width:400}}
             height ='90%'
-            placeholder="Share anything you'd like people to know about and learn from, whether it's fiction, a story or a fact ?"
+            placeholder="Share anything you'd like people to know about and learn from, whether it's fiction, a story or a fact "
             ></textarea>
             <div></div>
             <div>
